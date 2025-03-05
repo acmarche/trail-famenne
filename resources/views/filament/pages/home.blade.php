e12e186eb3327ed4f73903e27681cabb31de39f0
jfsenechal <jfsenechal@gmail.com>
2025-03-05 10:48:48 +0100

    mobile bof

--- resources/views/filament/pages/home.blade.php
+++ resources/views/filament/pages/home.blade.php
@@ -1,10 +1,12 @@
 <x-filament-panels::page>
     <section class="px-2 lg:px-12 flex mx-auto flex-col gap-3 items-start justify-items-center w-full lg:w-auto">
 
+        <h2 class="flex text-2xl lg:text-5xl font-semibold text-pink-500 mx-auto">Trail Famenne - Ardenne - 100Km</h2>
+
         @include('filament.pages.parts._banner_images')
 
         <div class="px-4 lg:px-12 flex mx-auto flex-col space-y-6 items-start justify-items-center">
-            <a href="{{route('information',['locale'=>'fr'])}}" class="flex flex-row items-center gap-4">
+            <a href="{{route('information',['locale'=>'fr'])}}" class="flex flex-row items-center gap-4 underline">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 40" width="60" height="40">
                     <rect width="20" height="40" fill="blue"/>
                     <rect x="20" width="20" height="40" fill="white"/>
@@ -13,7 +15,7 @@
                 Bienvenue au 100 km Famenne Ardenne
             </a>
 
-            <a href="{{route('information',['locale'=>'nl'])}}" class="flex flex-row items-center gap-4">
+            <a href="{{route('information',['locale'=>'nl'])}}" class="flex flex-row items-center gap-4 underline">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 40" width="60" height="40">
                     <rect width="60" height="40" fill="red"/>
                     <rect y="13.33" width="60" height="13.33" fill="white"/>
@@ -22,7 +24,7 @@
                 Welkom bij de 100 km Famenne Ardenne
             </a>
 
-            <a href="{{route('information',['locale'=>'en'])}}" class="flex flex-row items-center gap-4">
+            <a href="{{route('information',['locale'=>'en'])}}" class="flex flex-row items-center gap-4 underline">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 40" width="60" height="40">
                     <rect width="60" height="40" fill="white"/>
                     <rect x="25" width="10" height="40" fill="red"/>
@@ -31,7 +33,7 @@
                 Welcome to the 100 km Famenne Ardenne
             </a>
 
-            <a href="{{route('information',['locale'=>'de'])}}" class="flex flex-row items-center gap-4">
+            <a href="{{route('information',['locale'=>'de'])}}" class="flex flex-row items-center gap-4 underline">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 40" width="60" height="40">
                     <rect width="60" height="13.33" fill="black"/>
                     <rect y="13.33" width="60" height="13.33" fill="red"/>
--- resources/views/filament/pages/parts/_banner_images.blade.php
+++ resources/views/filament/pages/parts/_banner_images.blade.php
@@ -1,17 +1,14 @@
-<section class="py-4 lg:py-12">
-    <h2 class="text-2xl lg:text-5xl font-semibold text-pink-500">Trail Famenne - Ardenne - 100Km</h2>
-    <div class="mt-10 flex gap-8">
-        <img
-            class="w-20 lg:w-64 rotate-3 rounded-xl opacity-80 transition duration-500 hover:rotate-0 hover:scale-110 hover:opacity-100"
-            src="{{asset('images/banner/481014440_2465405970471191_8765295421514980802_n.jpg')}}"
-            alt=""/>
-        <img
-            class="w-20 lg:w-64 -rotate-3 rounded-xl opacity-80 transition duration-500 hover:rotate-0 hover:scale-110 hover:opacity-100"
-            src="{{asset('images/banner/481006538_2465406497137805_8703200934486112660_n.jpg')}}"
-            alt=""/>
-        <img
-            class="w-20 lg:w-64 rotate-3 rounded-xl opacity-80 transition duration-500 hover:rotate-0 hover:scale-110 hover:opacity-100"
-            src="{{asset('images/banner/481446759_2464512163893905_509118768460842177_n.jpg')}}"
-            alt=""/>
-    </div>
+<section class="py-4 lg:py-12 mx-auto flex gap-8">
+    <img
+        class="w-32 lg:w-64 rotate-3 rounded-xl opacity-80 transition duration-500 hover:rotate-0 hover:scale-110 hover:opacity-100"
+        src="{{asset('images/banner/481014440_2465405970471191_8765295421514980802_n.jpg')}}"
+        alt=""/>
+    <img
+        class="w-32 lg:w-64 -rotate-3 rounded-xl opacity-80 transition duration-500 hover:rotate-0 hover:scale-110 hover:opacity-100"
+        src="{{asset('images/banner/481006538_2465406497137805_8703200934486112660_n.jpg')}}"
+        alt=""/>
+    <img
+        class="w-32 lg:w-64 rotate-3 rounded-xl opacity-80 transition duration-500 hover:rotate-0 hover:scale-110 hover:opacity-100"
+        src="{{asset('images/banner/481446759_2464512163893905_509118768460842177_n.jpg')}}"
+        alt=""/>
 </section>
