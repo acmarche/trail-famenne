e2d64e14dfaa091d8bae3114af8199e67b0bcd05
jfsenechal <jfsenechal@gmail.com>
2025-03-06 10:45:11 +0100

    html

--- app/Filament/Pages/Homepage.php
+++ app/Filament/Pages/Homepage.php
@@ -2,34 +2,21 @@
 
 namespace App\Filament\Pages;
 
-use App\Models\Walker;
-use Filament\Actions\Action;
 use Filament\Pages\Page;
-use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\DB;
 use Filament\Support\Enums\MaxWidth;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Facades\App;
 
 class Homepage extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static string $view = 'filament.pages.home';
-
-    // protected static bool $shouldRegisterNavigation = false;
+    public string $locale = '';
 
     public function getTitle(): string|Htmlable
     {
         return ' ';
-        return __('invoices::messages.page.welcome.title');
     }
-
-    /**
-     * @var Collection<int, Walker> $walkers
-     */
-    public Collection $walkers;
-
-    public string $locale = '';
 
     public function __construct()
     {
@@ -44,11 +31,6 @@ class Homepage extends Page
     public static function canAccess(): bool
     {
         return true;
-    }
-
-    public function mount(): void
-    {
-        $this->walkers = DB::table('walkers')->get();
     }
 
     public function getLayout(): string
