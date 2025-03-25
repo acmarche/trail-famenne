40b7aa096894d41f7d9fb7d75bd873822c3737db
jfsenechal <jfsenechal@gmail.com>
2025-03-25 20:43:16 +0100

    view

--- app/Filament/AdminPanel/Resources/WalkerResource/Pages/ViewWalker.php
+++ app/Filament/AdminPanel/Resources/WalkerResource/Pages/ViewWalker.php
@@ -6,7 +6,7 @@ use App\Filament\AdminPanel\Resources\WalkerResource;
 use App\Models\Walker;
 use Filament\Actions;
 use Filament\Infolists;
-use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\Fieldset;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -26,6 +26,11 @@ class ViewWalker extends ViewRecord
         ];
     }
 
+    public function getTitle(): string
+    {
+        return $this->record->last_name.' '.$this->record->first_name ?? 'Empty name';
+    }
+
     public function infolist(Infolist $infolist): Infolist
     {
         return $infolist
@@ -34,32 +39,45 @@ class ViewWalker extends ViewRecord
                     ->inlineLabel(true)
                     ->columns(2)
                     ->schema([
-                        Infolists\Components\TextEntry::make('first_name')
-                            ->label('Prénom'),
-                        Infolists\Components\TextEntry::make('last_name')
-                            ->label('Nom'),
-                        Infolists\Components\TextEntry::make('email')
-                            ->icon('heroicon-m-envelope'),
-                        Infolists\Components\TextEntry::make('phone')
-                            ->label('Téléphone')
-                            ->icon('heroicon-m-phone'),
-                        Infolists\Components\TextEntry::make('street')
-                            ->label('Rue'),
-                        Infolists\Components\TextEntry::make('city')
-                            ->label('Localité'),
-                        Infolists\Components\TextEntry::make('country')
-                            ->label('Pays'),
-                        Infolists\Components\TextEntry::make('tshirt_size')
-                            ->label('T-shirt'),
-                        Infolists\Components\TextEntry::make('date_of_birth')
-                            ->label('Né le')
-                            ->dateTime(),
-                        Infolists\Components\TextEntry::make('is_paid')
-                            ->label('Payé')
-                            ->state(fn(Walker $walker) => $walker->isPaid() ? 'Oui' : 'Non')
-                            ->columnSpanFull(),
-                        Infolists\Components\TextEntry::make('club_name')
-                            ->label('Nom du club'),
+                        Fieldset::make('person')
+                            ->label('Coordonnées')
+                            ->schema([
+                                Infolists\Components\TextEntry::make('email')
+                                    ->icon('heroicon-m-envelope'),
+                                Infolists\Components\TextEntry::make('phone')
+                                    ->label('Téléphone')
+                                    ->icon('heroicon-m-phone'),
+                                Infolists\Components\TextEntry::make('city')
+                                    ->label('Localité'),
+                                Infolists\Components\TextEntry::make('date_of_birth')
+                                    ->label('Né le')
+                                    ->dateTime(),
+                            ]),
+                        Fieldset::make('contact')
+                            ->label('Autres informations')
+                            ->schema([
+                                Infolists\Components\TextEntry::make('country')
+                                    ->label('Pays'),
+                                Infolists\Components\TextEntry::make('tshirt_size')
+                                    ->label('T-shirt'),
+                                Infolists\Components\TextEntry::make('club_name')
+                                    ->label('Nom du club'),
+                            ]),
+                        Fieldset::make('rgpd')
+                            ->label('Rgpd')
+                            ->schema([
+                                Infolists\Components\TextEntry::make('newsletter_accepted')
+                                    ->label('Lettre d\'information'),
+                                Infolists\Components\TextEntry::make('regulation_accepted')
+                                    ->label('Accepte le règlement'),
+                                Infolists\Components\TextEntry::make('registration_date')
+                                    ->label('Date d\'inscription')
+                                    ->dateTime(),
+                                Infolists\Components\TextEntry::make('is_paid')
+                                    ->label('Payé')
+                                    ->state(fn(Walker $walker) => $walker->isPaid() ? 'Oui' : 'Non')
+                                    ->columnSpanFull(),
+                            ]),
                     ]),
             ]);
     }
