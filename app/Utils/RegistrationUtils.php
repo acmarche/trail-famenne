cbff489009516123345ce21a7605054e3aadc5ce
jfsenechal <jfsenechal@gmail.com>
2025-04-11 18:48:58 +0200

    fix

--- app/Utils/RegistrationUtils.php
+++ app/Utils/RegistrationUtils.php
@@ -8,6 +8,11 @@ class RegistrationUtils
 {
     public static function lastRegistrationId(): int
     {
-        return Walker::max('registration_id');
+        $id = Walker::max('registration_id');
+        if (!$id) {//first
+            return 1;
+        }
+
+        return $id;
     }
 }
