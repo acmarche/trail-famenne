5db1f0344f080dd72dd3680d609ab2ae87f7dd5f
jfsenechal <jfsenechal@gmail.com>
2025-08-04 16:28:38 +0200

    html

--- resources/views/filament/pages/tshirts.blade.php
+++ resources/views/filament/pages/tshirts.blade.php
@@ -1,7 +1,7 @@
 <x-filament-panels::page>
     <div class="mx-5 bg-white shadow-lg rounded-lg p-4">
         <div class="flex flex-row items-center justify-center">
-            <h1 class="text-3xl font-bold text-pink-500 text-center">Numérotations</h1>
+            <h1 class="text-3xl font-bold text-pink-500 text-center">Listing</h1>
         </div>
 
         {{ $this->table }}
